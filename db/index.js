7ea8380b89bf1db8e836d9dc85d9ca6955a5f574
Ashleyg5 <ashleygiraldo5@hotmail.com>
2023-04-29 14:22:01 -0400

    added proper structure, started coding index.js

---
+++ db/index.js
@@ -0,0 +1,16 @@
+const connection = require("./connection");
+
+class DB {
+    constructor(connection) {
+        this.connection = connection;
+    }
+
+    findAllEmployees() {
+        return this.connection.promise().query(
+
+        );
+    }
+
+}
+
+module.exports = new DB(connection);
---
+++ index.js
@@ -0,0 +1,49 @@
+const { prompt } = require("inquirer");
+const db = require("./db");
+require("console.table");
+
+init();
+
+function init() {
+    //logo display asciiart-logo zoom 4/27 2:53.47
+}
+
+function loadMainPrompts() {
+    prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                {
+                    name: "View All Employees",
+                    value: "VIEW_EMPLOYEES"
+                },
+                {
+                    name: "Add Employee",
+                    value: "ADD_EMPLOYEE"
+                },
+                {
+                    name: "Update Employee Role",
+                    value: "UPDATE_EMPLOYEE_ROLE"
+                },
+                {
+                    name: "View All Roles",
+                    value: "VIEW_ROLES"
+                },
+                {
+                    name: "Add Role",
+                    value: "ADD_ROLE"
+                },
+                {
+                    name: "View All Departments",
+                    value: "VIEW_DEPARTMENTS"
+                },
+                {
+                    name: "Add Department",
+                    value: "ADD_DEPARTMENT"
+                } 
+            ]
+        }
+    ])
+}
